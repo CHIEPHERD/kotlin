bb0cdd37bae68b7643516bf2fb084e60bc81ba7d
alex-lairan <lairana@free.fr>
2017-07-22 16:59:27 +0200

    Change switch scene mode

--- core/src/main/java/com/chiepherd/core/controllers/ApplicationController.kt
+++ core/src/main/java/com/chiepherd/core/controllers/ApplicationController.kt
@@ -1,22 +1,18 @@
 package com.chiepherd.core.controllers
 
 
+import com.chiepherd.core.extensions.javafx.event.stage
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.VBox
-import com.chiepherd.core.extensions.javafx.event.stage
+import com.chiepherd.core.services.MenuManager
 import javafx.event.ActionEvent
-import javafx.scene.Node
-import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.SplitPane
 import javafx.scene.layout.BorderPane
 import khttp.responses.Response
-import netscape.javascript.JSObject.getWindow
-import javafx.stage.Stage
 import java.net.URL
-
 
 abstract class ApplicationController : Initializable {
     val homeParent : VBox by lazy {
@@ -26,28 +22,26 @@ abstract class ApplicationController : Initializable {
         loader.load<VBox>()
     }
 
-    private fun contentLayout() : AnchorPane {
-        return (homeParent.lookup("SplitPane") as SplitPane).items[1] as AnchorPane
+    private fun sidePane(event: ActionEvent) : AnchorPane {
+        return (event.stage.scene.lookup("SplitPane") as SplitPane).items[0] as AnchorPane
+    }
+
+    private fun yieldPane(event: ActionEvent) : AnchorPane {
+        return (event.stage.scene.lookup("SplitPane") as SplitPane).items[1] as AnchorPane
     }
 
     private fun newScene(path : URL) : BorderPane {
         return FXMLLoader.load<BorderPane>(path)
     }
 
-    private fun homeScene() : Scene {
-        return Scene(homeParent)
-    }
+    protected fun switchScene(actionEvent : ActionEvent, resource : URL, menu : VBox = MenuManager.get("Logged")) {
+        val sidePane = sidePane(actionEvent)
+        sidePane.children.clear()
+        sidePane.children.add(menu)
 
-    protected fun switchScene(actionEvent : ActionEvent, resource : URL) {
-        println("resource $resource")
-        println("actionEvent $actionEvent")
-        val homeStage = actionEvent.stage
-        homeStage.scene = homeScene()
-        homeStage.show()
-
-        println(newScene(resource))
-
-        contentLayout().children.add(newScene(resource))
+        val yieldPane = yieldPane(actionEvent)
+        yieldPane.children.clear()
+        yieldPane.children.add(newScene(resource))
     }
 
     protected fun displayError(res: Response) {
