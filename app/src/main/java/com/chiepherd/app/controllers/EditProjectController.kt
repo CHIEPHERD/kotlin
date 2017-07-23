26203d20aa79714e6289280342610d128b210d29
amelie certin <amelie.certin@gmail.com>
2017-07-23 02:25:14 +0200

    Update a project

--- app/src/main/java/com/chiepherd/app/controllers/EditProjectController.kt
+++ app/src/main/java/com/chiepherd/app/controllers/EditProjectController.kt
@@ -1,6 +1,7 @@
 package com.chiepherd.app.controllers
 
 import com.chiepherd.core.controllers.ApplicationController
+import com.chiepherd.core.services.RabbitMQ
 import com.chiepherd.models.Project
 import com.jfoenix.controls.JFXTextArea
 import com.jfoenix.controls.JFXTextField
@@ -9,6 +10,7 @@ import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.VBox
+import org.json.JSONObject
 import java.net.URL
 import java.util.*
 
@@ -36,6 +38,18 @@ class EditProjectController(val project : Project) : ApplicationController() {
 
     fun onUpdate(actionEvent: ActionEvent?) {
         if (actionEvent == null) { return }
-        switchScene(actionEvent, javaClass.classLoader.getResource("chiepherd/views/project/projects.fxml"))
+        val json =
+                """{ "name": "${(formProject.children[1] as JFXTextField).text}",
+                 "label": "${(formProject.children[2] as JFXTextField).text}",
+                 "description": "${(formProject.children[3] as JFXTextArea).text}",
+                 "uuid": "${project.uuid}" }"""
+        val res = RabbitMQ.instance.sendMessage("chiepherd.project.update", json)
+
+        println(res)
+        // TODO: Check server errors
+        if (JSONObject(res).has("uuid")) {
+            Thread.sleep(1_000)
+            switchScene(actionEvent, javaClass.classLoader.getResource("chiepherd/views/project/projects.fxml"))
+        }
     }
 }
